51e755df822bd3069db8f7115eff0a4340147796
Mateus Urtassum <mateusurtassum@gmail.com>
2025-05-06 21:49:13 -0300

    using MiniCssExtractPlugin dependences to externalize css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development', //or production (create an minimalist code)
@@ -7,11 +8,17 @@ module.exports = {
         filename: 'main.js',
         path: __dirname + '/public' 
     },
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        })
+    ],
     module: {
         rules: [{
             test: /\.css$/, //read css files
             use: [
-                'style-loader', //add CSS to DOM using tag <style> inside public/main.js
+                MiniCssExtractPlugin.loader, //conflitande com o style-loader
+                //'style-loader', //add CSS to DOM using tag <style> inside public/main.js
                 'css-loader' //interpret @inport, url()...
             ]
         }]
